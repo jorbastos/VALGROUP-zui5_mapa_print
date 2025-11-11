1ca0966be7ea62fde975c36a3e503abe67e0b768
jorbastos <jorbastos@yahoo.com.br>
2025-11-11 15:03:56 -0300

    Adicionando Arquivos do Projeto

---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,167 @@
+sap.ui.define([
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
+], function (MessageToast, MessageBox) {
+    'use strict';
+
+    return {
+
+        onInit: function () {
+            this.attachEventsToTable();
+            this.addCustomToolbarContent();
+        },
+
+        attachEventsToTable: function () {
+            let oTable = this.getView().byId(this.getView().getId() + '--listReport').getTable(),
+                oPlugin = oTable.getPlugins()[0];
+
+            if (oPlugin) {
+                oPlugin.attachSelectionChange(this.onRowSelectionChange, this);
+            }
+        },
+
+        onRowSelectionChange: function (oEvent) {
+            let sActionsEnabled = false,
+                oModel = this.getOwnerComponent().getModel(),
+                oToolbarContent = this.getView().byId(this.getView().getId() + '--listReport').getToolbar().getContent(),
+                oContextSelected = this.extensionAPI.getSelectedContexts();
+
+            if (oContextSelected) {
+                let sPath = oContextSelected[0].getPath(),
+                    sObjeto = oModel.getProperty(sPath);
+                if (oContextSelected.length > 0 && sObjeto.IsMapaOk === 'S' && sObjeto.IsPesoOk === 'S') {
+                    sActionsEnabled = true;
+                }
+            }
+
+            for (var i = 0; i < oToolbarContent.length; i++) {
+                if (oToolbarContent[i].sId.includes('mapa--PDFCreateButton')) {
+                    oToolbarContent[i].setEnabled(sActionsEnabled);
+                }
+                if (oToolbarContent[i].sId.includes('mapa--action::cds_zsdui_ewm_c_mapa_carrega_print.cds_zsdui_ewm_c_mapa_carrega_print_Entities::print')) {
+                    oToolbarContent[i].setEnabled(sActionsEnabled);
+                }
+            }
+        },
+
+        addCustomToolbarContent: function () {
+            // Pega a toolbar da tabela
+            let oToolbar = this.getView().byId(this.getView().getId() + '--listReport').getToolbar(),
+                oToolbarContent = oToolbar.getContent();
+
+
+            for (var i = 0; i < oToolbarContent.length; i++) {
+                if (oToolbarContent[i].sId.includes('mapa--PDFCreateButton')) {
+                    oToolbarContent[i].setIcon('sap-icon://pdf-attachment');
+                }
+                if (oToolbarContent[i].sId.includes('mapa--action::cds_zsdui_ewm_c_mapa_carrega_print.cds_zsdui_ewm_c_mapa_carrega_print_Entities::print')) {
+                    oToolbarContent[i].setIcon('sap-icon://print');
+                }
+            }
+        },
+
+        onAfterRendering: function () {
+            let oTable = this.getView().byId(this.getView().getId() + '--listReport').getTable();
+            if (oTable) {
+                // 1º coluna fixa
+                oTable.setFixedColumnCount(1);
+            }
+        },
+
+        onBeforeRebindTableExtension: function (oEvent) {
+            const oBindingParams = oEvent.getParameter("bindingParams");
+
+            oBindingParams.parameters = oBindingParams.parameters || {};
+            // use this when you want to fetch the whole tree at once
+            oBindingParams.parameters.operationMode = "Client";
+            oBindingParams.parameters.treeAnnotationProperties = {
+                hierarchyLevelFor: "HierarchyLevel",
+                hierarchyNodeFor: "NodeID",
+                hierarchyParentNodeFor: "ParentNodeID",
+                hierarchyDrillStateFor: "DrillState"
+            };
+
+            // Para TreeTable começar Colapsado
+            oBindingParams.parameters.numberOfExpandedLevels = 0;
+            // Para TreeTable continuar Colapsado após Filtro
+            oEvent.getSource().getTable().collapseAll();
+        },
+
+        PDFCreate: function (oEvent) {
+            let oModel = this.getOwnerComponent().getModel(),
+                oModeli18n = this.getOwnerComponent().getModel("i18n"),
+                oContextSelected = this.extensionAPI.getSelectedContexts(),
+                sPath = oContextSelected[0].getPath(),
+                sObjeto = oModel.getProperty(sPath),
+                sRequest = '/pdf',
+                that = this;
+
+            let oKeys = {
+                "NodeID": sObjeto.NodeID
+            };
+
+            this.getView().setBusy(true);
+
+            oModel.callFunction(sRequest, {
+                method: "POST",
+                urlParameters: oKeys,
+                success: function (oData, oResponse) {
+                    that.getView().setBusy(false);
+                    that.downloadFile(JSON.parse(oResponse.body), oModeli18n);
+
+                }.bind(this),
+                error: function (oError) {
+                    that.getView().setBusy(false);
+                    var oErro = JSON.parse(oError.responseText);
+                    oErro = oErro.error.message.value;
+                    MessageBox.error(oErro);
+                }.bind(this)
+            });
+
+        },
+
+        downloadFile: function (pResult, pModeli18n) {
+
+            var i18nTexts = pModeli18n.getResourceBundle();
+
+            if (pResult.d.pdf.file_content === null || pResult.d.pdf.file_content === "") {
+                MessageBox.error(i18nTexts.getText("FormError"));
+                return;
+            }
+
+            try {
+                // Decodificar Base64
+                var byteCharacters = atob(pResult.d.pdf.file_content);
+                var byteNumbers = new Array(byteCharacters.length);
+                for (var i = 0; i < byteCharacters.length; i++) {
+                    byteNumbers[i] = byteCharacters.charCodeAt(i);
+                }
+                var byteArray = new Uint8Array(byteNumbers);
+
+                // Criar Blob
+                var blob = new Blob([byteArray], {
+                    type: pResult.d.pdf.mime_type
+                });
+
+                // Download
+                var link = document.createElement('a');
+                link.href = window.URL.createObjectURL(blob);
+                link.download = pResult.d.pdf.file_name;
+                link.style.display = 'none';
+                document.body.appendChild(link);
+                link.click();
+
+                // Cleanup
+                setTimeout(function () {
+                    document.body.removeChild(link);
+                    window.URL.revokeObjectURL(link.href);
+                }, 100);
+
+                return true;
+            } catch (error) {
+                MessageBox.error(i18nTexts.getText("DownloadError") + error.message);
+            }
+
+        },
+    }
+});
